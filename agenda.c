aead48578b51a4577f63d69c12dfb6148a89efaf
Gustavo Taques <gustavotaques@hotmail.com>
2024-06-24 00:02:04 -0300

    finish delete contacts

--- agenda.c
+++ agenda.c
@@ -2,55 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM_NOME 100
+#define TAM_TEL 20
+
 typedef struct {
-    char nome[50];
-    char telefone[11];
+    char nome[TAM_NOME];
+    char telefone[TAM_TEL];
 } Contato;
 
-void adicionarContatos(FILE *arquivo) {
-    Contato contato;
-
-    printf("Digite o nome do contato: ");
-    fgets(contato.nome, 100, stdin);
-    contato.nome[strlen(contato.nome)-1] = '\0';
-
-    printf("Digite o telefone do contato: ");
-    fgets(contato.telefone, 11, stdin);
-    contato.telefone[strlen(contato.telefone)-1] = '\0';
-
-    fwrite(&contato, sizeof(Contato), 1, arquivo);
-}
-
-void consultarContatos(FILE *arquivo) {
-
-}
-
-void excluirContatos(FILE *arquivo) {
-
-}
-
+void adicionarContatos(FILE *arquivo);
+int consultarContatos(FILE *arquivo);
+void excluirContatos(FILE *arquivo);
 
 int main(void){
-    FILE *arquivo;
     int op;
+    FILE *arquivo;
 
-    arquivo = fopen("agenda.bin", "wb+");
-    if (arquivo == NULL){
-        printf("Erro ao abrir o arquivo.\n");
-        return 1;
+    arquivo = fopen("agenda.bin", "rb+");
+    if(arquivo == NULL){
+        arquivo = fopen("agenda.bin", "wb+");
+        if(arquivo == NULL){
+            printf("Erro ao abrir o arquivo.\n");
+            return 1;
+        }
     }
 
-    printf("BEM VINDO A AGENDA ELETRONICA!\n\n");
+    printf("BEM VINDO A AGENDA ELETRONICA!\n");
 
     do {
-        printf("1 - Adicionar um contato\n");
+        printf("\n1 - Adicionar um contato\n");
         printf("2 - Consultar um contato\n");
         printf("3 - Excluir um contato\n");
         printf("4 - Fechar a agenda\n\n");
 
-        printf("Digite a opcao desejada: ");
-        scanf("%i", &op);
-        fflush(stdin);
+        printf("Digite a opcao desejada: ");       
+        scanf("%d", &op);
+        getchar();
         printf("\n");
 
         switch (op) {
@@ -76,4 +63,43 @@ int main(void){
     fclose(arquivo);
 
     return 0;
+}
+
+void adicionarContatos(FILE *arquivo) {
+    Contato contato;
+
+    printf("Digite o nome do contato: ");
+    fgets(contato.nome, TAM_NOME, stdin);
+    contato.nome[strlen(contato.nome)-1] = 0;
+
+    printf("Digite o telefone do contato: ");
+    fgets(contato.telefone, TAM_TEL, stdin);
+    contato.telefone[strlen(contato.telefone)-1] = 0;
+
+    fseek(arquivo, 0, SEEK_END);
+    fwrite(&contato, sizeof(Contato), 1, arquivo);
+}
+
+int consultarContatos(FILE *arquivo) {
+    Contato contato;
+    char nome[TAM_NOME];
+
+    printf("Digite o nome do contato a ser consultado: ");
+    fgets(nome, TAM_NOME, stdin);
+    nome[strlen(nome)-1] = 0;
+    
+
+    fseek(arquivo, 0, SEEK_SET);
+    while(fread(&contato, sizeof(Contato), 1, arquivo)){
+        if(strcmp(contato.nome, nome) == 0){
+            printf("\nContato encontrado!\n\nNome: %s\nTelefone: %s\n\n", contato.nome, contato.telefone);
+            return 0;
+        }
+    }
+    printf("\nContato nao encontrado! :(\n");
+    return -1;
+}
+
+void excluirContatos(FILE *arquivo) {
+
 }
